bfd708589984d21803f75f01715658815d9710a5
austin-cravven <funguycoolguy529@gmail.com>
2017-04-02 13:04:53 -0500

    UnityGame

---
+++ MedGameTest/Assets/PlayerScripts/HealthSystemScript.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthSystemScript : MonoBehaviour {
+    public float MaxH { get; set; }
+    public float CurrentH { get; set; }
+    public Slider HealthB; 
+	void Start ()
+    {
+        MaxH = 100.0f;
+
+        CurrentH = MaxH;
+
+        HealthB.value = CalcHealth(); 
+
+    }
+	
+	void Update ()
+    {
+		if (Input.GetButtonDown("DamageButton"))
+        {
+            DamagePlayer(10.0f);
+        }
+	}
+
+    void DamagePlayer(float damageAmount)
+    {
+        CurrentH -= damageAmount;
+        HealthB.value = CalcHealth();
+
+        if (CurrentH <= 0)
+        {
+            Death();
+        }
+    }
+
+     float CalcHealth()
+    {
+        return CurrentH / MaxH; 
+        
+    }
+
+
+
+    void Death()
+    {
+        CurrentH = 0; 
+        Destroy(gameObject); 
+    }
+
+
+
+
+}
